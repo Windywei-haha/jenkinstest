e13766a3e9ae28f327ef3ba6b742dd239f0105b8
“windy” <418809213@qq.com>
2021-08-04 22:04:30 +0800

    the first hello world

---
+++ src/test/java/Logon.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+/**
+ * @Project: jenkinstest
+ * @Author: 阿线现
+ * @Create: 2021-08-04 21:07
+ * @Desc：
+ **/
+public class Logon {
+    @Test
+    public void logon(){
+        System.out.println("登录成功");
+    }
+}
---
+++ src/test/java/Register.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+/**
+ * @Project: jenkinstest
+ * @Author: 阿线现
+ * @Create: 2021-08-04 21:06
+ * @Desc：
+ **/
+public class Register {
+    @Test
+    public void register(){
+        System.out.println("注册成功");
+    }
+}
